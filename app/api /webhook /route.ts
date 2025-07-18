099def35eb86643dc40d769cd8fe1adc7fb33c1c
Frankfurt1991 <topdonhi551@gmail.com>
2025-07-18 13:37:58 +0700

    Delete app/api /webhook  directory

--- app/api /webhook /route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from 'next/server';
-
-export async function POST(req: Request) {
-  return NextResponse.json({ message: 'OK' }, { status: 200 });
-}
-
-export async function GET() {
-  return NextResponse.json({ message: 'Method Not Allowed' }, { status: 405 });
-}
