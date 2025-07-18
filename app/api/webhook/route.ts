d63a2838afd194ba15304b2ae73162e465566793
Frankfurt1991 <topdonhi551@gmail.com>
2025-07-18 13:49:30 +0700

    Update route.ts

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -1,9 +1,7 @@
-import { NextResponse } from 'next/server';
-
 export async function POST(req: Request) {
-  return NextResponse.json({ message: 'OK' }, { status: 200 });
+  return new Response('OK', { status: 200 });
 }
 
 export async function GET() {
-  return NextResponse.json({ message: 'Method Not Allowed' }, { status: 405 });
+  return new Response('Method Not Allowed', { status: 405 });
 }
